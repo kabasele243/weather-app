fe678672e685a9a055dd452632bb74402da23f9c
Franck Kabasele <kabasele467@gmail.com>
2020-12-27 19:10:38 -0500

    Add: Partials templates

--- server/app.js
+++ server/app.js
@@ -1,35 +1,39 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 //Define paths for Express config
 const publicDirectory = path.join(__dirname, './public')
-const viewsPath = path.join(__dirname, './templates')
+const viewsPath = path.join(__dirname, './templates/views')
+const partialsPath = path.join(__dirname, './templates/partials')
 
 
 //Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+ console.log(partialsPath)
 
 //Static directory
 app.use(express.static(publicDirectory))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: "Makenna"
+        title: "Weather"
     })
 })
 
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: "Makenna"
+        title: "Help"
     })
 })
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: "Makenna"
+        title: "About"
     })
 })
 
